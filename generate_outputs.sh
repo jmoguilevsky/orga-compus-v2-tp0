73cb70d2ffce766b0e67dc39a79e0d7c0933e62a
jmoguilevsky <jmoguilevsky@gmail.com>
2019-09-17 19:07:20 -0300

    update tests

--- generate_outputs.sh
+++ generate_outputs.sh
@@ -2,10 +2,15 @@
 
 FILES=$(ls tests)
 
-rm -rf success_results
-mkdir success_results
+rm -rf results
+mkdir results
 
 for f in $FILES
 do
-    cat success_tests/$f | ./output.c > success_results/$f
+    cat tests/$f | ./output.c > results/$f
 done
+
+./output.c -h > results/help_menu_1.txt
+./output.c --help > results/help_menu_2.txt
+./output.c -V > results/version_1.txt
+./output.c --version > results/version_2.txt
--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-FILES=$(ls success_tests)
+FILES=$(ls tests)
 
 for f in $FILES
 do
     cat tests/$f | ./output.c > temp.txt
     echo "Running test for $f"
-    ./compare_files.sh temp.txt success_results/$f
+    ./compare_files.sh temp.txt results/$f
     echo
 done
 
